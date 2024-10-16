88bc47da8d78cb781fa79436a9034c94fcb55f03
minfan Chen <mingfan1202@gmail.com>
2024-10-17 01:01:20 +0800

    feat: react實戰影音/後台版型導入
    feat: react實戰影音/後台版型導入
    
    feat: react實戰影音/後台版型導入

---
+++ src/page/backend/Home/index.js
@@ -0,0 +1,14 @@
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+const BHome = () => {
+
+  return (
+    <Box component="section" sx={{ p: 2 }}>
+      <Typography variant="h4" component="div">公告資訊</Typography>
+      只有產品列表XD....
+    </Box>
+  );
+}
+
+export default BHome;
