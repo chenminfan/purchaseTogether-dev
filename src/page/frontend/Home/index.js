b3defcfa80fa0521964ec8cc1e305a1e3fc2c1b5
minfan Chen <mingfan1202@gmail.com>
2024-10-17 00:41:19 +0800

    feat: react實戰影音/導入 Bootstrap

---
+++ src/page/frontend/Home/index.js
@@ -0,0 +1,16 @@
+import { Outlet } from 'react-router-dom'
+import { LoginContentProvider } from '../../../components/module/LoginProvider';
+import Header from '../../../components/Header';
+
+const Home = () => {
+
+  return (
+    <LoginContentProvider>
+      <Header headerTitle="react cart" >
+      </Header>
+      <main className="App"><Outlet /></main>
+    </LoginContentProvider>
+  );
+}
+
+export default Home;
