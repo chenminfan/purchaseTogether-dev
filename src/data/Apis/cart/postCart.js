6f1517ac888b4f50dc7627a4851286e53fb76a49
minfan Chen <mingfan1202@gmail.com>
2024-10-16 22:25:23 +0800

    feat: react實戰影音/加入環境變數

---
+++ src/data/Apis/cart/postCart.js
@@ -0,0 +1,11 @@
+// import axios from 'axios';
+// axios.defaults.baseURL = process.env.REACT_APP_API_URL
+
+// async function postCartApi() {
+//   try {
+//     return await axios.post(`/v2/api/${process.env.REACT_APP_API_PATH}/cart`)
+//   } catch (error) {
+//     throw error;
+//   }
+// }
+// export { postCartApi }
