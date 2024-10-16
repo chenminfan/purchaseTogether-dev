873afeceec16ec8d4b110b939d989d725c4a8d6f
minfan Chen <mingfan1202@gmail.com>
2024-10-17 00:59:59 +0800

    feat: react實戰影音/開發登入畫面

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Routes, Route } from 'react-router-dom';
+import Home from './page/frontend/Home';
+import Login from './page/frontend/Login';
+import Dashboard from './page/backend/Dashboard';
+import BackendProduct from './page/backend/BackendProduct';
 import './assets/all.scss'
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+export default function App() {
+  useEffect(() => {
 
-export default App;
+  }, [])
+  return (
+    <Routes>
+      <Route exact path='/' element={<Home />}>
+        <Route path='/login' element={<Login />}></Route>
+      </Route>
+      <Route path='/backend' element={<Dashboard />}>
+        <Route path='/backend/product' element={<BackendProduct />}></Route>
+
+      </Route>
+      {/* 暫放 */}
+      {/* <Route path='/reactDaily' element={<ReactDailyApp />}></Route> */}
+    </Routes>
+  )
+}
