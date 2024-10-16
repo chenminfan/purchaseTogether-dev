146078091d1746513acf249f4fea789d8a0d3a0f
minfan Chen <mingfan1202@gmail.com>
2024-10-17 01:00:43 +0800

    feat: react實戰影音/Cookie 的運作概念

--- src/data/Apis/login/postLogin.js
+++ src/data/Apis/login/postLogin.js
@@ -5,7 +5,10 @@ axios.defaults.baseURL = process.env.REACT_APP_API_URL
 async function postLoginApi(data) {
   try {
     const res = await axios.post('/v2/admin/signin', data)
-
+    const { token, expired } = res.data;
+    // 儲存token
+    document.cookie = `hexToken=${token}; expires=${new Date(expired)}`
+    axios.defaults.headers.common['Authorization'] = token
     return res
   } catch (error) {
     throw error;
