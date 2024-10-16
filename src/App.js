7285bc67f5a0c1381dd48042a874a3671f1d0269
minfan Chen <mingfan1202@gmail.com>
2024-10-17 01:01:20 +0800

    feat: react實戰影音/後台版型導入
    feat: react實戰影音/後台版型導入

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Routes, Route } from 'react-router-dom';
+import BHome from './page/backend/Home';
 import Home from './page/frontend/Home';
 import Login from './page/frontend/Login';
 import Dashboard from './page/backend/Dashboard';
-import BackendProduct from './page/backend/BackendProduct';
+import BackendProducts from './page/backend/BackendProducts';
 import './assets/all.scss'
 
 export default function App() {
-  useEffect(() => {
-
-  }, [])
   return (
     <Routes>
       <Route exact path='/' element={<Home />}>
         <Route path='/login' element={<Login />}></Route>
       </Route>
       <Route path='/backend' element={<Dashboard />}>
-        <Route path='/backend/product' element={<BackendProduct />}></Route>
+        <Route path='/backend' element={<BHome />}></Route>
+        <Route path='/backend/product' element={<BackendProducts />}></Route>
 
-      </Route>
+      </Route >
       {/* 暫放 */}
       {/* <Route path='/reactDaily' element={<ReactDailyApp />}></Route> */}
-    </Routes>
+    </Routes >
   )
 }
