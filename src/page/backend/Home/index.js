4bbe5c37f0a7ce9fbc1a8c027c56216818fc62b9
minfan Chen <mingfan1202@gmail.com>
2024-10-24 01:22:25 +0800

    feat: react實戰影音/刪除單一商品

--- src/page/backend/Home/index.js
+++ src/page/backend/Home/index.js
@@ -4,7 +4,7 @@ import Box from '@mui/material/Box';
 const BHome = () => {
 
   return (
-    <Box component="section" sx={{ p: 2 }}>
+    <Box component="section">
       <Typography variant="h4" component="div">公告資訊</Typography>
       只有產品列表XD....
     </Box>
