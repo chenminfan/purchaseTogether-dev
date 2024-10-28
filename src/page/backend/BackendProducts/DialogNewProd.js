7e1119693eae36386fed504671ebae04d7ec16ae
minfan Chen <mingfan1202@gmail.com>
2024-10-29 03:10:27 +0800

    feat: react實戰影音(延伸)/全部及筆數刪除功能

--- src/page/backend/BackendProducts/DialogNewProd.js
+++ src/page/backend/BackendProducts/DialogNewProd.js
@@ -119,13 +119,13 @@ export default function DialogNewProd(props) {
   useEffect(() => {
     if (prodType === 'create') {
       setFormData({
-        title: '',
-        category: '',
-        content: '',
+        title: 'DialogNewProd DialogNewProd',
+        category: 'DialogNewProd DialogNewProd',
+        content: 'DialogNewProd',
         origin_price: 3000,
         price: 300,
         unit: '個',
-        description: '',
+        description: 'DialogNewProd',
         is_enabled: 1,
         imageUrl: 'https://images.unsplash.com/photo-1525088553748-01d6e210e00b?q=80&w=1752&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
       })
