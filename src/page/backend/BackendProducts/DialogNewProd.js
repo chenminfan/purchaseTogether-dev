0cb7711da24a02d928cca9347246ffb0133d15bb
minfan Chen <mingfan1202@gmail.com>
2024-10-29 18:25:13 +0800

    feat: react實戰影音(延伸)/刪除功能(useContext 與 useReducer)

--- src/page/backend/BackendProducts/DialogNewProd.js
+++ src/page/backend/BackendProducts/DialogNewProd.js
@@ -129,7 +129,7 @@ export default function DialogNewProd(props) {
         is_enabled: 1,
         imageUrl: 'https://images.unsplash.com/photo-1525088553748-01d6e210e00b?q=80&w=1752&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
       })
-    } else if (prodType === 'edit' || prodType === 'delete') {
+    } else if (prodType === 'edit') {
       setFormData(tampData);
     }
 
