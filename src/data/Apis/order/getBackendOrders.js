e03edeeb70633d732f3129f74bbef669acf7690f
minfan Chen <mingfan1202@gmail.com>
2024-10-30 04:04:36 +0800

    feat: react實戰影音/完成後台

---
+++ src/data/Apis/order/getBackendOrders.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+axios.defaults.baseURL = process.env.REACT_APP_API_URL
+
+async function getBackendOrdersApi(page) {
+  try {
+    return await axios.get(`/v2/api/${process.env.REACT_APP_API_PATH}/admin/orders?page=${page}`)
+  } catch (error) {
+    throw error;
+  }
+}
+export { getBackendOrdersApi }
--- src/data/Apis/order/getOrders.js
+++ src/data/Apis/order/getOrders.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 axios.defaults.baseURL = process.env.REACT_APP_API_URL
 
-async function getOrderApi() {
+async function getOrdersApi() {
   try {
     return await axios.get(`/v2/api/${process.env.REACT_APP_API_PATH}/orders`)
   } catch (error) {
     throw error;
   }
 }
-export { getOrderApi }
+export { getOrdersApi }
