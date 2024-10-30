7e48b130cbd0e399512c3b4c06de7f2e2ce7408a
minfan Chen <mingfan1202@gmail.com>
2024-10-31 05:36:04 +0800

    feat: react實戰影音(調整)/ts導入

---
+++ src/data/Apis/login/postLogin.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+axios.defaults.baseURL = process.env.REACT_APP_API_URL
+
+
+async function postLoginApi(data) {
+  try {
+    const res = await axios.post('/v2/admin/signin', data)
+    const { token, expired } = res.data;
+    // 儲存token
+    document.cookie = `hexToken=${token}; expires=${new Date(expired)}`
+    axios.defaults.headers.common['Authorization'] = token
+    return res
+  } catch (error: any) {
+    throw error;
+  }
+}
+export { postLoginApi }
---
+++ src/data/Apis/order/postBackendOrders.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+axios.defaults.baseURL = process.env.REACT_APP_API_URL
+
+async function postBackendOrdersApi(prodType, data) {
+  let api = `/v2/api/${process.env.REACT_APP_API_PATH}/admin/orders`
+  let method = 'post'
+  try {
+    if (prodType === 'edit') {
+      api = `/v2/api/${process.env.REACT_APP_API_PATH}/admin/orders/${data.id}`
+      method = "put"
+    } else if (prodType === 'delete' || prodType === 'allDelete') {
+      api = `/v2/api/${process.env.REACT_APP_API_PATH}/admin/orders/${data.id}`
+      method = "delete"
+    }
+    return await axios[method](api, { data: data })
+
+  } catch (error: any) {
+    throw error;
+  }
+}
+export { postBackendOrdersApi }
