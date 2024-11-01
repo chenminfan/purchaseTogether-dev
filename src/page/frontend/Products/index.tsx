ec06a96cc9d41655d50e334f515dd7c698c65a6a
minfan Chen <mingfan1202@gmail.com>
2024-11-02 01:20:02 +0800

    feat: react實戰影音/(前台)前台餐點列表
    feat: react實戰影音/(前台)前台餐點列表

--- src/page/frontend/Products/index.tsx
+++ src/page/frontend/Products/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import Prods from '@components/frontend/Prods'
 import { getProductsApi, getProductsAllApi } from '@api/Apis/product/getProducts'
 import { ProductsType } from '@typeTS/Products'
@@ -16,6 +16,8 @@ export default function Products() {
     has_next: false,
     category: ''
   })
+  const isLoadingRef = useRef(true)
+  const [loadingPage, setLoadingPage] = useState<boolean>(true);
   const [categoryId, setCategoryId] = useState<string>('')
   const getProds = async (getPage = 1, category = '') => {
     const prodRes = await getProductsApi(getPage, category);
@@ -28,24 +30,35 @@ export default function Products() {
   const Id = category.find((item) => item)
   useEffect(() => {
     getProds()
+    isLoadingRef.current = false
+    setLoadingPage(false)
   }, [])
 
   const handleClick = (item) => {
-    getProds(1, item)
+    if (item === 'all') {
+      getProds(1, '')
+    } else {
+      getProds(1, item)
+    }
     setCategoryId(item)
   }
-
   return (
     <div className="prods_page">
       <nav className="navbar navbar-expand-lg">
         <ul className="navbar-nav navbar-custom-scroll">
-          {category.map((item, index) => (
+          <li className='nav-item'
+            onClick={() => {
+              handleClick('all')
+            }}>
+            <a className={`nav-link ${categoryId === 'all' ? 'active' : ''}`} role="link" aria-label="prods-link" href="#/prods" ><i className="bi bi-signpost-split-fill"></i></a>
+          </li>
+          {category.map((item) => (
             <li className='nav-item'
               key={item}
               onClick={() => {
                 handleClick(item)
               }}>
-              <a className={`nav-link ${item === (categoryId ? categoryId : Id) ? 'active' : ''}`} href="#" >{item}</a>
+              <a className={`nav-link ${item === (categoryId ? categoryId : Id) ? 'active' : ''}`} role="link" aria-label="prods-link" href="#/prods" >{item}</a>
             </li>
           ))}
         </ul>
@@ -57,7 +70,7 @@ export default function Products() {
             <div className="prods_box">
               {prods.map((item) => {
                 return (
-                  <Prods key={item.id} prod={item} />
+                  <Prods key={item.id} prod={item} isLoading={loadingPage} />
                 )
               })}
             </div>
