98094d4c7534759209d1ff978b0e4d1674c19f7d
minfan Chen <mingfan1202@gmail.com>
2024-11-02 01:20:02 +0800

    feat: react實戰影音/(前台)商品

---
+++ src/components/frontend/Prods/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { FrontendProductsType } from '@typeTS/frontend/FProducts'
+import './prods.scss'
+
+type ProdsType = {
+  prod: FrontendProductsType,
+}
+export default function Prods(props: ProdsType) {
+  const { prod } = props
+  return (
+    <div className="card prods">
+      <div className="card-body">
+        <div className="prods-content">
+          <h5 className="card-title"><a href="#">{prod.title}</a></h5>
+          <div className="prods-price">
+            <div className="prods-item">
+              <div className="title">特價</div>
+              <span>${prod.price.toLocaleString('zh-TW')}</span>
+            </div>
+            <div className="prods-item">
+              <div className="title">原價</div>
+              <span className='origin-price'>${prod.origin_price.toLocaleString('zh-TW')}</span>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="img_box">
+          <a href="#"><img className="card-img-top" src={prod.imageUrl} alt={prod.title} />
+          </a>
+        </div>
+
+
+        <div className="prods-tool">
+          <button type="button" className="btn btn-primary prods-btn-add"><span className='prods-cart-icon'>
+            <i className="bi bi-cart-check-fill"></i>
+          </span></button>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/page/frontend/ProductDetail/index.tsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from 'react'
+import Prods from '@components/frontend/Prods'
+import { getProductsApi, getProductsAllApi } from '@api/Apis/product/getProducts'
+import { FrontendProductsType } from '@typeTS/frontend/FProducts'
+import { PaginationType } from '@typeTS/PaginationType'
+import Pagination from '@components/backend/Pagination';
+import './productDetail.scss'
+
+export default function ProductDetail() {
+  const [prods, setProds] = useState<FrontendProductsType[]>([])
+  const [prodAll, setProdAll] = useState<FrontendProductsType[]>([])
+  const [page, setPage] = useState<PaginationType>({
+    total_pages: 0,
+    current_page: 1,
+    has_pre: false,
+    has_next: false,
+    category: ''
+  })
+  const [categoryId, setCategoryId] = useState<string>('')
+  const getProds = async (getPage = 1, category = '') => {
+    const prodRes = await getProductsApi(getPage, category);
+    const prodAllRes = await getProductsAllApi();
+    setProds(prodRes.data.products)
+    setProdAll(prodAllRes.data.products)
+    setPage(prodRes.data.pagination)
+  }
+  const category = Array.from(new Set(prodAll.map((item) => item.category)))
+  const Id = category.find((item) => item)
+  useEffect(() => {
+    getProds()
+  }, [])
+
+  const handleClick = (item) => {
+    getProds(1, item)
+    setCategoryId(item)
+  }
+
+  return (
+    <div className="prods_page">
+      <nav className="navbar navbar-expand-lg">
+        <ul className="navbar-nav navbar-custom-scroll">
+          {category.map((item, index) => (
+            <li className='nav-item'
+              key={item}
+              onClick={() => {
+                handleClick(item)
+              }}>
+              <a className={`nav-link ${item === (categoryId ? categoryId : Id) ? 'active' : ''}`} href="#" >{item}</a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+
+      <div className='container-fluid px-2'>
+        <div className="row">
+          <div className="col">
+            <div className="prods_box">
+              {prods.map((item) => {
+                return (
+                  <Prods key={item.id} prod={item} />
+                )
+              })}
+            </div>
+          </div>
+        </div>
+
+
+        <Pagination page={page} getPagination={getProds} pageLink="#" />
+      </div>
+
+    </div>
+  )
+}
