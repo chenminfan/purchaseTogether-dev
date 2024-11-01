849c6164323910b2698b23306a176fabb8952227
minfan Chen <mingfan1202@gmail.com>
2024-11-02 01:20:02 +0800

    feat: react實戰影音/(前台)商品

--- src/data/Apis/product/getProducts.ts
+++ src/data/Apis/product/getProducts.ts
@@ -1,11 +1,18 @@
 import axios from 'axios';
 axios.defaults.baseURL = process.env.REACT_APP_API_URL
 
-async function getProductsApi() {
+async function getProductsApi(page, category) {
   try {
-    return await axios.get(`/v2/api/${process.env.REACT_APP_API_PATH}/products`)
+    return await axios.get(`/v2/api/${process.env.REACT_APP_API_PATH}/products?category=${category}&page=${page}`)
   } catch (error: any) {
     throw error;
   }
 }
-export { getProductsApi }
+async function getProductsAllApi() {
+  try {
+    return await axios.get(`/v2/api/${process.env.REACT_APP_API_PATH}/products/all`)
+  } catch (error: any) {
+    throw error;
+  }
+}
+export { getProductsApi, getProductsAllApi }
