0cc3ea73fbd25917433d3a61b3163b69a40a26d4
minfan Chen <mingfan1202@gmail.com>
2024-11-01 22:27:49 +0800

    feat: react實戰影音/完成商品後台(分類,搜尋)

--- src/data/Apis/product/getBackendProducts.ts
+++ src/data/Apis/product/getBackendProducts.ts
@@ -8,4 +8,11 @@ async function getBackendProductsApi(page: number) {
     throw error;
   }
 }
-export { getBackendProductsApi }
+async function getBackendProductsCategoryApi(page: number, category: string) {
+  try {
+    return await axios.get(`/v2/api/${process.env.REACT_APP_API_PATH}/admin/products?category=${category}&page=${page}`)
+  } catch (error: any) {
+    throw error;
+  }
+}
+export { getBackendProductsApi, getBackendProductsCategoryApi }
