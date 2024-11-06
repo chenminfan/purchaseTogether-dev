54c816b424e070bb8c628ebe6aef3f8d12f04516
minfan Chen <mingfan1202@gmail.com>
2024-11-06 18:55:35 +0800

    feat: react實戰影音/(前台)商品詳細資訊

--- src/page/frontend/Frontend.tsx
+++ src/page/frontend/Frontend.tsx
@@ -1,6 +1,7 @@
 import React, { Outlet } from 'react-router-dom'
 import { LoginContentProvider } from '@provider/LoginProvider';
 import Header from '@components/frontend/Header';
+import Footer from '@components/frontend/Footer';
 import 'bootstrap/dist/js/bootstrap.js'
 import './frontend.scss'
 
@@ -11,6 +12,7 @@ export default function Frontend() {
       <main className='frontend'>
         <Outlet />
       </main>
+      <Footer />
     </LoginContentProvider>
   )
 }
