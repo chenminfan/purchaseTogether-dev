b34db440e4a4fe644f9e7a38e7e4c3a3112c49db
minfan Chen <mingfan1202@gmail.com>
2024-11-10 22:02:35 +0800

    feat: react實戰影音/(前台)加入購物車

--- src/page/frontend/Frontend.tsx
+++ src/page/frontend/Frontend.tsx
@@ -1,18 +1,23 @@
 import React, { Outlet } from 'react-router-dom'
 import { LoginContentProvider } from '@provider/LoginProvider';
+import SnackbarProvider from '@provider/SnackbarProvider'
 import Header from '@components/frontend/Header';
 import Footer from '@components/frontend/Footer';
+import Toasts from '@components/frontend/Toasts';
 import 'bootstrap/dist/js/bootstrap.js'
 import './frontend.scss'
-
 export default function Frontend() {
+
   return (
     <LoginContentProvider>
-      <Header headerTitle="react cart" />
-      <main className='frontend'>
-        <Outlet />
-      </main>
-      <Footer />
+      <SnackbarProvider>
+        <Header headerTitle="react cart" cartData={cartData} />
+        <main className='frontend'>
+          <Outlet />
+        </main>
+        <Footer />
+        <Toasts />
+      </SnackbarProvider>
     </LoginContentProvider>
   )
 }
