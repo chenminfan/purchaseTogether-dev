e4b3b956b05a36998c304e7854174e0a2a6a921e
minfan Chen <mingfan1202@gmail.com>
2024-11-11 03:53:56 +0800

    feat: react實戰影音/(前台)購物車頁面製作,移除餐點品項

--- src/components/frontend/InputStepper/index.tsx
+++ src/components/frontend/InputStepper/index.tsx
@@ -1,41 +1,49 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './inputStepper.scss'
 
-export default function InputStepper(props) {
-  const { num } = props
-  const [stepperNum, setStepperNum] = useState<number>(num)
+type StepperType = {
+  inputStepperNum: number,
+  setInputStepperNum: (value: number) => void
+  inputStepperOnChange?: (e: any) => void
+  handleClickCut?: () => void
+  handleClickAdd?: () => void
+}
+export default function InputStepper(props: StepperType) {
+  const { inputStepperNum, setInputStepperNum, inputStepperOnChange = () => { } } = props;
   const handleAdd = (number) => {
-    setStepperNum(++number)
+    setInputStepperNum(++number)
   }
   const handleCut = (number) => {
-    if (stepperNum > 1 && stepperNum !== 0) {
-      setStepperNum(--number)
+    if (inputStepperNum > 1 && inputStepperNum !== 0) {
+      setInputStepperNum(--number)
     }
   }
 
   const handleChange = (e) => {
     if (!/\d*/.test(e.target.value)) return
-    setStepperNum(e.target.value)
+    setInputStepperNum(e.target.value)
+    inputStepperOnChange(e.target.value)
   }
   return (
     <div className="inputStepper rounded">
       <div className="inputStepper-reduce">
         <button className="btn btn-outline-dark border-0 py-2" type="button" id="button-addon1"
           onClick={() => {
-            handleCut(stepperNum)
+            handleCut(inputStepperNum)
           }}
         >
           <i className="bi bi-dash-lg"></i>
         </button>
       </div>
-      <input type="number" className="form-control border-0 text-center my-auto shadow-none bg-transparent" value={stepperNum} onChange={(e) => {
-        handleChange(e)
-      }} />
+      <input type="number" className="form-control border-0 text-center my-auto shadow-none bg-transparent" value={inputStepperNum}
+        onChange={(e) => {
+          handleChange(e)
+        }} />
       <div className="inputStepper-increase">
         <button className="btn btn-outline-dark border-0 py-2" type="button" id="button-addon2"
           onClick={() => {
 
-            handleAdd(stepperNum)
+            handleAdd(inputStepperNum)
           }}
         >
           <i className="bi bi-plus"></i>
