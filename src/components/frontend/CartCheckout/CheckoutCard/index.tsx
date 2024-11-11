6e67f9905d688c5efffc97acd1820a5257c7e488
minfan Chen <mingfan1202@gmail.com>
2024-11-11 14:08:57 +0800

    feat: react實戰影音/(前台)更新餐點數量(沒使用select)

---
+++ src/components/frontend/CartCheckout/CheckoutCard/index.tsx
@@ -0,0 +1,131 @@
+import React, { useState, useContext, useRef, useEffect } from 'react'
+import LazyLoadImg from "@components/hook/LazyLoadImage";
+import Stepper from '@components/frontend/Stepper'
+import { ProductsType } from '@typeTS/Products';
+import { postCartApi } from '@api/Apis';
+import { DialogContent } from '@provider/DialogProvider/DialogContent'
+import './checkoutCard.scss'
+
+type Props = {
+  cart: {
+    final_total: number,
+    id: string,
+    product: ProductsType,
+    qty: number,
+    total: number
+  },
+  isSuccess: boolean,
+  checkout: () => void,
+}
+
+export default function CheckoutCard(props: Props) {
+  const { cart, isSuccess, checkout } = props
+  const [stepperNum, setStepperNum] = useState(cart.qty)
+  const [state, dispatch] = useContext<any>(DialogContent);
+  const stepperValue = useRef(0)
+  const isLoadingRef = useRef(true)
+  const [loadingPage, setLoadingPage] = useState<boolean>(false);
+  const [stepperEdit, setStepperEdit] = useState<boolean>(true);
+
+  const handleDelete = async (cartData) => {
+    try {
+      const res = await postCartApi('delete', cartData)
+      checkout()
+      dispatch({
+        type: 'DIALOG_MESSAGE',
+        snackbar: {
+          message: res.data.message,
+          snackbarState: res.data.success,
+        }
+      })
+    } catch (error: any) {
+      dispatch({
+        type: 'DIALOG_MESSAGE',
+        snackbar: {
+          message: error?.response?.data?.message,
+          snackbarState: error?.response?.data?.success,
+        }
+      })
+    }
+  }
+  const getCart = async (cart, qty) => {
+    const data = {
+      id: cart.id,
+      product_id: cart.product.id,
+      qty: qty,
+    }
+    isLoadingRef.current = loadingPage
+    setLoadingPage(true)
+    try {
+      const res = await postCartApi('edit', data)
+      isLoadingRef.current = loadingPage
+      setLoadingPage(false)
+      checkout()
+      dispatch({
+        type: 'DIALOG_MESSAGE',
+        snackbar: {
+          message: res.data.message,
+          snackbarState: res.data.success,
+        }
+      })
+    } catch (error: any) {
+      isLoadingRef.current = loadingPage
+      setLoadingPage(false)
+      dispatch({
+        type: 'DIALOG_MESSAGE',
+        snackbar: {
+          message: error?.response?.data?.message,
+          snackbarState: error?.response?.data?.success,
+        }
+      })
+    }
+  }
+
+  useEffect(() => {
+    stepperValue.current = stepperNum
+  }, [stepperNum])
+
+  return (
+    <div className="checkout checkout-card" >
+      <div className="card-image">
+        <button className="btn btn-primary card-btn btn-sm" type="button">
+          {isSuccess ? (<i className="bi bi-bookmark-heart-fill"></i>) : (<i className="bi bi-bookmark-heart"></i>)}
+        </button>
+        <div className="img_box">
+          <LazyLoadImg className="card-img-top" src={cart.product.imageUrl} alt={cart.product.title} />
+        </div>
+      </div>
+      <div className="card-info">
+        <button className="btn text-primary card-btn btn-sm" type="button" onClick={() => { handleDelete(cart) }}><i className="bi bi-x-lg"></i></button>
+        <div className="card-title">{cart.product.title}</div>
+
+        <div className="card-content">
+          <div className="card-stepper">
+            <Stepper
+              stepperNum={stepperNum}
+              setStepperNum={setStepperNum}
+              handleClickAdd={(addNumber) => {
+                getCart(cart, addNumber)
+              }}
+              handleClickCut={(cutNumber) => {
+                getCart(cart, cutNumber)
+              }}
+              isDisabled={loadingPage}
+              isEdit={cart.qty !== stepperNum && stepperEdit}
+              handleClickSubmit={(inputNumber) => {
+                getCart(cart, inputNumber)
+              }}
+              handleInputChange={
+                (e) => {
+                  setStepperNum(Number(e))
+                }
+              }
+            />
+          </div>
+          <div className="card-price">NT$ {cart.total.toLocaleString('zh-TW')}</div>
+        </div>
+
+      </div>
+    </div>
+  )
+}
--- src/components/frontend/Stepper/index.tsx
+++ src/components/frontend/Stepper/index.tsx
@@ -1,56 +1,100 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './stepper.scss'
 
 
 type StepperType = {
   stepperNum: number,
   setStepperNum: (value: number) => void
-  stepperOnChange?: (e: any) => void
-  handleClickCut?: () => void
-  handleClickAdd?: () => void
+  handleClickCut?: (value: number) => void
+  handleClickAdd?: (value: number) => void,
+  handleInputChange?: (e: any) => void,
+  handleClickSubmit?: (e: any) => void,
+  setStepperEdit?: (value: boolean) => void,
+  isDisabled?: boolean,
+  isEdit?: boolean,
 }
 export default function Stepper(props: StepperType) {
-  const { stepperNum, setStepperNum, stepperOnChange = () => { } } = props;
+  const { stepperNum,
+    setStepperNum,
+    handleClickAdd = () => { },
+    handleClickCut = () => { },
+    handleInputChange = () => { },
+    handleClickSubmit = () => { },
+    isDisabled,
+    isEdit,
+    setStepperEdit = () => { }, } = props;
+
+  const stepperValue = useRef(0)
   const handleAdd = (number) => {
+    stepperValue.current = number + 1
     setStepperNum(++number)
+    handleClickAdd(number)
   }
   const handleCut = (number) => {
     if (stepperNum > 1 && stepperNum !== 0) {
+      stepperValue.current = number - 1
       setStepperNum(--number)
+      handleClickCut(number)
     }
   }
 
   const handleChange = (e) => {
-    if (!/\d*/.test(e.target.value)) return
-    setStepperNum(e.target.value)
-    stepperOnChange(e.target.value)
+    const { value } = e.target
+    if (!/\d*/.test(value)) {
+      return
+    } else if (value.length > 4) {
+      return
+    }
+    setStepperNum(value)
+    handleInputChange(value)
   }
+
+  const handleSubmit = () => {
+    handleClickSubmit(stepperNum)
+  }
+  useEffect(() => {
+    stepperValue.current = stepperNum
+  }, [stepperNum])
   return (
-    <div className="stepper rounded"  >
+    <div className={`stepper rounded ${isEdit ? 'stepper-edit' : ''}`}>
       <div className="stepper-reduce">
-        <button className="btn btn-outline-dark border-0 py-2" type="button" id="button-addon1"
+        <button className="btn btn-outline-dark border-0 py-2" type="button"
           onClick={() => {
             handleCut(stepperNum)
           }}
+          disabled={isEdit || isDisabled}
         >
           <i className="bi bi-dash-lg"></i>
         </button>
       </div>
-      <input type="number" className="form-control border-0 text-center my-auto shadow-none bg-transparent" value={stepperNum}
-        onChange={(e) => {
-          handleChange(e)
-        }}
+      <input type="number"
+        className="form-control border-0 text-center my-auto shadow-none bg-transparent"
+        aria-label={isDisabled ? 'Disabled' : ''}
+        value={stepperNum}
+        onChange={(e) => { handleChange(e) }}
+        disabled={isDisabled}
       />
       <div className="stepper-increase">
         <button className="btn btn-outline-dark border-0 py-2" type="button" id="button-addon2"
           onClick={() => {
-
             handleAdd(stepperNum)
           }}
+          disabled={isEdit || isDisabled}
         >
           <i className="bi bi-plus"></i>
         </button>
       </div>
+      {isEdit && <div className='stepper-tool'>
+        <button className="btn btn-sm btn-dark border-0 py-2" type="button"
+          onClick={() => {
+            setStepperEdit(true)
+            handleSubmit()
+          }}
+          disabled={isDisabled}
+        >
+          確認修改
+        </button>
+      </div>}
     </div >
   )
 }
