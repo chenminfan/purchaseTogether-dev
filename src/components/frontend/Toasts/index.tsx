bbb982b72d988fc773025cb799498917bbc5d200
minfan Chen <mingfan1202@gmail.com>
2024-11-13 16:57:07 +0800

    feat: react實戰影音/toasts 元件優化

--- src/components/frontend/Toasts/index.tsx
+++ src/components/frontend/Toasts/index.tsx
@@ -1,20 +1,16 @@
 import React, { useContext } from 'react'
-import { SnackbarContent } from '@provider/SnackbarProvider/SnackbarContent'
+import { SnackbarContent, handleSnackbarClose } from '@provider/SnackbarProvider/SnackbarContent'
 import './toast.scss'
 
 export default function Toasts() {
   const [state, dispatch] = useContext<any>(SnackbarContent);
-  const handleClose = (dispatch: any) => {
-    dispatch({
-      type: 'DIALOG_CLOSE',
-    });
-  }
 
   return (
     <div className={`toast fade ${state.snackbarState ? 'text-bg-primary' : 'text-bg-danger'} position-fixed p-1 ${state.snackbarOpen ? 'show' : ''}`} role="alert" aria-live="assertive" aria-atomic="true" >
       <div className="d-flex">
         <div className="toast-body">{state.message}</div>
-        <button type="button" className="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close" onClick={handleClose}></button>
+        <button type="button" className="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"
+          onClick={() => { handleSnackbarClose(dispatch) }}></button>
       </div>
     </div>
   )
