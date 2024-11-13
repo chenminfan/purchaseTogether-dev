baf4ef634e68b9160cc84492ae7b5c840149be0d
minfan Chen <mingfan1202@gmail.com>
2024-11-13 23:49:21 +0800

    feat: react實戰影音/(前台)首頁完成(審核後調整)

--- src/components/frontend/InputFrom/Input.tsx
+++ src/components/frontend/InputFrom/Input.tsx
@@ -3,23 +3,27 @@ type TextAreaType = {
   id: string,
   labelText: string,
   type: string,
+  placeholder?: string,
   rules?;
+  value?: string | number,
   register;
   errors;
 }
 
 export default function TextArea(props: TextAreaType) {
-  const { id, labelText, type, rules, register, errors } = props;
+  const { id, labelText, type, rules, register, errors, placeholder, value } = props;
   return (
-    <div className='my-3'>
-      <label htmlFor={id} className='form-label'>
+    <div className={labelText ? 'my-3' : ''}>
+      {labelText && <label htmlFor={id} className='form-label'>
         {labelText}
-      </label>
+      </label>}
       <input
         id={id}
         type={type}
         {...register(id, rules)}
+        placeholder={placeholder}
         className={`form-control ${errors[id] && 'is-invalid'}`}
+        value={value}
       />
       {errors[id] && (
         <div className='invalid-feedback'>{errors?.[id]?.message}</div>
