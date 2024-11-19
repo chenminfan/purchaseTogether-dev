99f71891c27d5325ed9066aec7f25fc372bd5acf
minfan Chen <mingfan1202@gmail.com>
2024-11-20 03:08:31 +0800

    feat: react實戰影音/(前台)header調整

--- src/components/frontend/Header/index.tsx
+++ src/components/frontend/Header/index.tsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import LogoBUY from '@components/frontend/LogoBUY';
 import './header.scss';
 
 export default function Header(props) {
-  const { headerLink = '#', headerTitle, cartData } = props;
-
+  const { headerLink = '#', cartData, trackList } = props;
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+  const handleMouseLeave = () => setIsNavCollapsed(!isNavCollapsed);
   return (
     <header>
-      <nav className="navbar navbar-expand-lg">
+      <nav className="navbar navbar-expand-lg" onMouseLeave={handleMouseLeave}>
         <div className="container-fluid">
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle-Link" role="button">
+          <button className={`navbar-toggler fade ${!isNavCollapsed ? '' : 'collapsed'}`} type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded={!isNavCollapsed ? true : false} aria-label="Toggle-Link" role="button" onClick={handleNavCollapse} >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse navbar-box" id="navbarTogglerDemo01">
+
+          <div className={`collapse fade navbar-collapse navbar-box collapse ${!isNavCollapsed ? 'show' : ''}`} id="navbarTogglerDemo01">
             <ul className="navbar-nav">
               <li className="nav-item">
                 <a className="nav-link" href="#/main/prods" aria-label="prods-category" role="link">
@@ -24,11 +27,26 @@ export default function Header(props) {
                 <a className="nav-link" href="#/main/track" aria-label="prods-category" role="link">
                   <i className="bi bi-bookmark-heart-fill"></i>
                   追蹤商品
+                  {trackList.length ? (
+                    <span className="badge rounded-pill text-bg-danger navbar-carIcon-number">
+                      {trackList.length ? trackList.length : 0}
+                    </span>
+                  ) : (
+                    <span className="badge rounded-pill text-bg-danger navbar-carIcon-number">
+                      <span className={trackList.length === 0 ? "" : "visually-hidden"}>等你追</span>
+                    </span>
+                  )}
                 </a>
               </li>
-              <li className="nav-item">
+              <li className="nav-item nav-item-mobile">
+                <a className="nav-link" href="#/main/order" aria-label="prods-category" role="link">
+                  <i className="bi bi-card-checklist"></i>
+                  訂單明細
+                </a>
+              </li>
+              <li className="nav-item nav-item-mobile">
                 <a className="nav-link" href="#/main/login" aria-label="prods-category" role="link">
-                  <i className="bi bi-signpost-split-fill"></i>
+                  <i className="bi bi-window-sidebar"></i>
                   後台管理
                 </a>
               </li>
@@ -55,12 +73,12 @@ export default function Header(props) {
                   )}
                 </a>
               </li>
-              <li className="nav-item">
+              <li className="nav-item nav-item-mobile">
                 <a className="nav-link" href="#/main/order" aria-label='backend-user' role="link">
                   <i className="bi bi-card-checklist"></i>
                 </a>
               </li>
-              <li className="nav-item">
+              <li className="nav-item nav-item-mobile">
                 <a className="nav-link" href="#/main/login" aria-label='backend-user' role="link">
                   <i className="bi bi-window-sidebar"></i>
                 </a>
