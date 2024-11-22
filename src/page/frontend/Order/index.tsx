6c6b9203ae76d9d8f09a14b25b78783365370c87
minfan Chen <mingfan1202@gmail.com>
2024-11-23 01:00:18 +0800

    feat: react實戰影音/(前台)增加訂單明細(調整)

--- src/page/frontend/Order/index.tsx
+++ src/page/frontend/Order/index.tsx
@@ -10,7 +10,6 @@ export default function Order() {
     try {
       const codeRes = await getOrdersApi()
       setOrderData(codeRes.data.orders)
-      console.log(codeRes)
     } catch (error) {
     }
   }
@@ -27,35 +26,38 @@ export default function Order() {
             <h4 className="fw-bold mb-4">我的訂單</h4>
             <div className='order-box'>
               <div className="accordion" id="accordionExample">
-                {orderData.map((order, orderIndex) => (
-                  <div className="accordion-item" key={`order_${order.id}`}>
-                    <h2 className="accordion-header">
-                      <button className={`accordion-button ${orderIndex === 0 ? '' : 'collapsed'}`} type="button" data-bs-toggle="collapse" data-bs-target={`#collapseOne${orderIndex}`} aria-expanded={orderIndex === 0 ? 'true' : 'false'} aria-controls={`collapseOne${orderIndex}`}>
-                        {dataValue(order.create_at)}
-                      </button>
-                    </h2>
-                    <div id={`collapseOne${orderIndex}`} className={`accordion-collapse collapse ${orderIndex === 0 ? 'show' : ''}`} data-bs-parent="#accordionExample">
-                      <div className="accordion-body">
-                        <ul className="list-group list-group-flush">
-                          {/* <li className="list-group-item active" aria-current="true"></li> */}
-                          <li className="list-group-item">{order.id}</li>
-                          <li className="list-group-item">{nameValue(order.user.name)}</li>
-                          <li className="list-group-item">{telValue(order.user.tel)}</li>
-                          <li className="list-group-item">{Object.values(order.products).map((item) => (
-                            <div className='order-prod' key={`prod_${item.id}`}>
-                              <div className="order-prod-title">{item.product.title}</div>
-                              <div className="order-prod-qty">× {item.qty}{item.product.unit}</div>
-                            </div>
-                          ))}</li>
-                        </ul>
+                {orderData.map((order, orderIndex) => {
+                  return (
+                    <div className="accordion-item" key={`order_${order.id}`}>
+                      <h2 className="accordion-header">
+                        <button className={`accordion-button ${orderIndex === 0 ? '' : 'collapsed'}`} type="button" data-bs-toggle="collapse" data-bs-target={`#collapseOne${orderIndex}`} aria-expanded={orderIndex === 0 ? 'true' : 'false'} aria-controls={`collapseOne${orderIndex}`}>
+                          {dataValue(order.create_at)}
+                        </button>
+                      </h2>
+                      <div id={`collapseOne${orderIndex}`} className={`accordion-collapse collapse ${orderIndex === 0 ? 'show' : ''}`} data-bs-parent="#accordionExample">
+                        <div className="accordion-body">
+                          <ul className="list-group list-group-flush">
+                            <li className="list-group-item">訂單明細：{order.id}</li>
+                            <li className="list-group-item">訂單日期：{nameValue(order.user.name)}</li>
+                            <li className="list-group-item">聯絡人電話：{telValue(order.user.tel)}</li>
+                            <li className="list-group-item">訂單付款狀態：<a href={`#/main/cart/pay/${order.id}`}>{order.is_paid ? '已付款' : '未付款'}</a></li>
+                            <li className="list-group-item">
+                              {order.products && Object.values(order.products).map((item) => {
+                                return (
+                                  <div className='order-prod' key={`prod_${item.product.id}`}>
+                                    <div className="order-prod-title"><span className='order-prod-note'>{item.product.category}</span> ／{item.product.title}</div>
+                                    <div className="order-prod-qty">× {item.qty}{item.product.unit}</div>
+                                  </div>
+                                )
+                              })}
+                            </li>
+                          </ul>
+                        </div>
                       </div>
+
                     </div>
-
-                  </div>
-
-
-
-                ))}
+                  )
+                })}
               </div>
             </div>
           </div>
