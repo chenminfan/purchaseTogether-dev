f9c258999a9d0a62eb3427f13ff5689378636624
minfan Chen <mingfan1202@gmail.com>
2024-11-29 15:24:01 +0800

    feat: react實戰影音/(前台)調整會員登入跳轉問題,補上無訂單狀態及調整輪播圖

--- src/components/frontend/CartStep/CartStepItem.tsx
+++ src/components/frontend/CartStep/CartStepItem.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { LoginContext } from '@provider/LoginProvider/LoginContext'
 import './cartStep.scss'
 
 type Props = {
@@ -10,11 +11,13 @@ type Props = {
 
 export default function CartStepItem(props: Props) {
   const { title, info, active, disabled } = props
+  const { USER_TOKEN } = useContext<any>(LoginContext)
   return (
     <div className={`cartStep-item ${active ? 'is-active' : ''} ${disabled ? 'is-disabled' : ''}`}>
       <div className="cartStep-spot"></div>
       <div className="cartStep-box">
         <div className="cartStep-title">{title}</div>
+        {(USER_TOKEN === '') && title === "購物車結帳" && <div className="cartStep-title text-danger">您尚未登入會員！</div>}
         {info && <div className="cartStep-info">{info}</div>}
       </div>
     </div>
