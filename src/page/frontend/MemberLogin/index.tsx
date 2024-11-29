4b6afd5e296a9e679eac19b86998cde148d48503
minfan Chen <mingfan1202@gmail.com>
2024-11-29 15:24:01 +0800

    feat: react實戰影音/(前台)調整會員登入跳轉問題,補上無訂單狀態及調整輪播圖

--- src/page/frontend/MemberLogin/index.tsx
+++ src/page/frontend/MemberLogin/index.tsx
@@ -17,7 +17,7 @@ export default function MemberLogin() {
     handleSubmit,
     formState: { errors },
   } = useForm()
-  const { auth, user, getMember, loginState, setLoginState, token } = useContext<any>(LoginContext)
+  const { auth, USER_MEMBER, getMember, loginState, setLoginState, USER_TOKEN } = useContext<any>(LoginContext)
   const navigate = useNavigate()
   const nav = [{ navName: '登入', navID: 'login' },
   { navName: '註冊', navID: 'create' }]
@@ -116,10 +116,10 @@ export default function MemberLogin() {
   })
 
   useEffect(() => {
-    if (token !== '' && user !== null) {
+    if ((USER_MEMBER !== null || USER_MEMBER !== '') && USER_TOKEN !== '') {
       navigate('/main/member')
     }
-  }, [user, token])
+  }, [USER_MEMBER, USER_TOKEN])
 
   return (
     <section className="memberLogin_page">
